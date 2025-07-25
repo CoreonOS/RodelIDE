3f802dc41d3c12aad6e81854374920bcea3cbdd4
DrxcoDev2 <drxcofn2023@gmail.com>
2025-07-25 11:23:24 +0200

    commit

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_simple_ide_FILE /home/drxco/RodelIDE/simple-ide/build/simple_ide)
-set(__QT_DEPLOY_TARGET_simple_ide_TYPE EXECUTABLE)
--- build/CMakeFiles/simple_ide.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for simple_ide.
-# This may be replaced when dependencies are built.
