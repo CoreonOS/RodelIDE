b38d07241d3d0de363b46bb5a1c45e09a21cf05f
DrxcoDev2 <drxcofn2023@gmail.com>
2025-07-25 11:21:41 +0200

    commit

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_simple_ide_FILE /home/drxco/RodelIDE/simple-ide/build/simple_ide)
+set(__QT_DEPLOY_TARGET_simple_ide_TYPE EXECUTABLE)
---
+++ build/CMakeFiles/simple_ide.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_ide.dir/link.d"
+  "CMakeFiles/simple_ide.dir/src/MainWindow.cpp.o"
+  "CMakeFiles/simple_ide.dir/src/MainWindow.cpp.o.d"
+  "CMakeFiles/simple_ide.dir/src/main.cpp.o"
+  "CMakeFiles/simple_ide.dir/src/main.cpp.o.d"
+  "simple_ide"
+  "simple_ide.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_ide.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
